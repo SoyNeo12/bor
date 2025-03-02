8f0b1e4e8483f872501353a196a5cd5f0cc30d38
SADDDDDDDDDDDDJKSADNASKCA <130333212+SoyNeo12@users.noreply.github.com>
2025-03-02 16:17:26 -0300

    Update index.js

--- ledigoholagoodbye/index.js
+++ ledigoholagoodbye/index.js
@@ -1,19 +1,2 @@
-const express = require('express');
-const { haxballBot } = require('./examples/script');
-const { discordBot } = require('./examples/discord/iniciar');
-
-const app = express();
-
-app.get('/start/script1', (req, res) => {
-    haxballBot;
-    res.send('script1.js iniciado');
-});
-
-app.get('/start/script2', (req, res) => {
-    discordBot;
-    res.send('script2.js iniciado');
-});
-
-app.listen(3000, () => {
-    console.log('Servidor Express escuchando en http://localhost:3000');
-});
+require('./examples/script');
+require('./examples/discord/iniciar');
